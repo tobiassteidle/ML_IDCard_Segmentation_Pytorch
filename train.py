7b95c3f798da30fe28669f2124bfc854f165438b
Tobias Steidle <tobias.steidle@softwaredev.de>
2019-11-06 16:00:10 +0100

    feat: optimized train.py

--- train.py
+++ train.py
@@ -19,11 +19,6 @@ IMAGE_SIZE = (256, 256)
 
 
 def dice_coef(y_true, y_pred, smooth=1):
-    """
-    Dice = (2*|X & Y|)/ (|X|+ |Y|)
-         =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
-    ref: https://arxiv.org/pdf/1606.04797v1.pdf
-    """
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
     return (2. * intersection + smooth) / (K.sum(K.square(y_true), -1) + K.sum(K.square(y_pred), -1) + smooth)
 
@@ -59,26 +54,27 @@ def main():
     train_generator = zip(train_image_generator, train_mask_generator)
     val_generator = zip(val_image_generator, val_mask_generator)
 
+    # build model
     model = models.UUNET(input_size=(256, 256, 3))
-
     optimizer = Adam(lr=1E-5, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     model.compile(loss=dice_coef_loss, optimizer=optimizer, metrics=[dice_coef])
 
-    checkpoint = ModelCheckpoint('weights/', monitor=dice_coef, verbose=1, save_best_only=True, mode='max')
+    # configure callbacks
+    checkpoint = ModelCheckpoint("model.h5", monitor=dice_coef, verbose=1, save_best_only=True,
+                                 save_weights_only=False, mode='max')
 
-    now = time.strftime("%Y%m%d_%H%M%S")
-    tensorboard = TensorBoard(log_dir='./logs/' + now, histogram_freq=0, write_graph=True, write_images=True)
-    earlystopping = EarlyStopping(monitor=dice_coef, verbose=1, min_delta=0.01, patience=3, mode='max')
+    earlystopping = EarlyStopping(monitor=dice_coef, verbose=1, min_delta=0.01, patience=3,
+                                  mode='max', restore_best_weights=True)
 
-    callbacks_list = [checkpoint, tensorboard, earlystopping]
+    tensorboard = TensorBoard(log_dir='./logs/' + time.strftime("%Y%m%d_%H%M%S"), histogram_freq=0,
+                              write_graph=True, write_images=True)
 
-    results = model.fit_generator(train_generator, epochs=NO_OF_EPOCHS,
+    # train model
+    model.fit_generator(train_generator, epochs=NO_OF_EPOCHS,
                                   steps_per_epoch=(NO_OF_TRAINING_IMAGES // BATCH_SIZE),
                                   validation_data=val_generator,
                                   validation_steps=(NO_OF_VAL_IMAGES // BATCH_SIZE),
-                                  callbacks=callbacks_list)
-    model.save('Model.h5')
-
+                                  callbacks=[checkpoint, earlystopping, tensorboard])
 
 if __name__ == '__main__':
     main()
