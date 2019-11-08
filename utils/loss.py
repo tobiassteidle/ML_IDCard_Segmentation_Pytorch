37fa7cc582bafe9ecd0f9877433c2282ccb0314a
Tobias Steidle <tobias.steidle@softwaredev.de>
2019-11-08 10:41:30 +0100

    feat: reorganized code.

---
+++ utils/loss.py
@@ -0,0 +1,30 @@
+from keras import backend as K
+from utils import metrics
+
+def dice_coef_loss(y_true, y_pred):
+    return 1 - dice_coef(y_true, y_pred)
+
+def iou_loss(true, pred):
+    intersection = true * pred
+    notTrue = 1 - true
+    union = true + (notTrue * pred)
+    return K.sum(intersection)/K.sum(union)
+
+def jaccard_distance_loss(y_true, y_pred, smooth=100):
+    """
+    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+
+    The jaccard distance loss is usefull for unbalanced datasets. This has been
+    shifted so it converges on 0 and is smoothed to avoid exploding or disapearing
+    gradient.
+
+    Ref: https://en.wikipedia.org/wiki/Jaccard_index
+
+    @url: https://gist.github.com/wassname/f1452b748efcbeb4cb9b1d059dce6f96
+    @author: wassname
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
