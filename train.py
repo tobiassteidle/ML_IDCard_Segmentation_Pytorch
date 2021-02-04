65f446617a1e79f75b3df2e3ab11b13da4d48a41
Tobias Steidle <tobias.steidle@softwaredev.de>
2021-02-04 08:45:11 +0100

    feat: transcribe training to pytorch.

--- train.py
+++ train.py
@@ -1,92 +1,188 @@
-import numpy as np
+import sys
 import os
-import random as rn
-import tensorflow as tf
+import numpy as np
 import time
-from keras.callbacks import EarlyStopping
-from keras.callbacks import ModelCheckpoint
-from keras.callbacks import ReduceLROnPlateau
-from keras.callbacks import TensorBoard
-from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import load_model
+import random
+from os import walk
+import pathlib
+from PIL import Image
+
+import torch
+from torch import nn
+from torch.optim import Adam
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 import models
-from utils import metrics
-
-NO_OF_TRAINING_IMAGES = len(os.listdir('dataset/train/train_frames/image'))
-NO_OF_VAL_IMAGES = len(os.listdir('dataset/train/val_frames/image'))
+from utils.metrics import multi_acc, iou_score
 
 NO_OF_EPOCHS = 500
 BATCH_SIZE = 8
-
 IMAGE_SIZE = (256, 256)
 
 SEED = 230
-rn.seed(SEED)
-np.random.seed(SEED)
-tf.set_random_seed(SEED)
 
+CHECKPOINT_PATH = pathlib.Path("./pretrained/model_checkpoint.pt")
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def seed_torch(seed=SEED):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    #torch.backends.cudnn.deterministic = True
+
+class SegmentationImageDataset(Dataset):
+    def __init__(self, image_dir, mask_dir):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        _, _, self.filenames = next(walk(image_dir))
+
+    @classmethod
+    def preprocess(cls, pil_img):
+        pil_img = pil_img.convert('L')
+
+        pil_img = pil_img.resize(IMAGE_SIZE)
+        img_nd = np.array(pil_img)
+
+        if len(img_nd.shape) == 2:
+            img_nd = np.expand_dims(img_nd, axis=2)
+
+        # HWC to CHW
+        img_trans = img_nd.transpose((2, 0, 1))
+        if img_trans.max() > 1:
+            img_trans = img_trans / 255
+
+        return img_trans
+
+    def __getitem__(self, idx):
+        image = Image.open(self.image_dir + '/' + self.filenames[idx])
+        mask = Image.open(self.mask_dir + '/' + self.filenames[idx])
+
+        image = self.preprocess(image)
+        mask = self.preprocess(mask)
+
+        image = torch.from_numpy(image).type(torch.FloatTensor)
+        mask = torch.from_numpy(mask).type(torch.FloatTensor)
+
+        return image, mask
+
+    def __len__(self):
+        return len(self.filenames)
+
+def saveCheckpoint(filename, epoch, model, optimizer, batchsize):
+    checkpoint = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        "batch_size": batchsize,
+    }
+
+    # save all important stuff
+    torch.save(checkpoint , filename)
+
+def train(model, data_loader, criterion, optimizer, scheduler, num_epochs=5):
+    logdir = './logs/' + time.strftime("%Y%m%d_%H%M%S")
+    logdir = os.path.join(logdir)
+    pathlib.Path(logdir).mkdir(parents=True, exist_ok=True)
+    tb_writer = SummaryWriter(log_dir=logdir)
+
+    best_acc = 0.0
+    best_loss = sys.float_info.max
+
+    for epoch in range(num_epochs):
+        result = []
+
+        for phase in ['train', 'val']:
+            if phase == 'train':  # put the model in training mode
+                model.train()
+            else:
+                # put the model in validation mode
+                model.eval()
+
+            # keep track of training and validation loss
+            running_loss = 0.0
+            running_iou = 0.0
+            running_corrects = 0.0
+
+            for (data, labels) in data_loader[phase]:
+                # load the data and target to respective device
+                (data, labels) = (data.to(device), labels.to(device))
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    # feed the input
+                    output = model(data)
+
+                    # calculate the loss
+                    loss = criterion(output, labels.to(torch.float32))
+
+                    if phase == 'train':
+                        # backward pass: compute gradient of the loss with respect to model parameters
+                        loss.backward()
+
+                        optimizer.step()
+
+                        # zero the grad to stop it from accumulating
+                        optimizer.zero_grad()
+
+                # statistics
+                running_loss += loss.item() * data.size(0)
+                running_iou = iou_score(output, labels)
+                running_corrects = multi_acc(output, labels)
+
+            if phase == 'train':
+                scheduler.step(running_iou)
+
+            # epoch statistics
+            epoch_loss = running_loss / len(data_loader[phase].dataset)
+            epoch_iou = running_iou / len(data_loader[phase].dataset)
+            epoch_acc = running_corrects / len(data_loader[phase].dataset)
+
+            result.append('{} Loss: {:.4f} Acc: {:.4f} IoU: {:.4f}'.format(phase, epoch_loss, epoch_acc, epoch_iou))
+
+            tb_writer.add_scalar('Loss/' + phase, epoch_loss, epoch)
+            tb_writer.add_scalar('Accuracy/' + phase, epoch_acc, epoch)
+
+            if phase == 'val' and epoch_loss < best_loss:
+                best_acc = epoch_acc
+                best_loss = epoch_loss
+                saveCheckpoint(CHECKPOINT_PATH, epoch, model, optimizer, BATCH_SIZE)
+                print('Checkpoint saved - Loss: {:.4f} Acc: {:.4f} IoU: {:.4f}'.format(epoch_loss, epoch_acc, epoch_iou))
+
+
+        print(result)
+
+    print('-----------------------------------------')
+    print('Final Result: Loss: {:.4f} Acc: {:.4f}'.format(best_loss, best_acc))
+    print('-----------------------------------------')
 
 def main():
-    train_datagen = ImageDataGenerator(rescale=1. / 255)
-    train_image_generator = train_datagen.flow_from_directory('./dataset/train/train_frames',
-                                                              target_size=IMAGE_SIZE,
-                                                              class_mode=None,
-                                                              batch_size=BATCH_SIZE,
-                                                              color_mode='grayscale',
-                                                              seed=SEED)
+    seed_torch()
 
-    train_mask_generator = train_datagen.flow_from_directory('dataset/train/train_masks',
-                                                             target_size=IMAGE_SIZE,
-                                                             class_mode=None,
-                                                             batch_size=BATCH_SIZE,
-                                                             color_mode='grayscale',
-                                                             seed=SEED)
+    print('Create datasets...')
+    train_dataset = SegmentationImageDataset('./dataset/train/train_frames/image', './dataset/train/train_masks/image')
+    validation_dataset = SegmentationImageDataset('./dataset/train/val_frames/image', './dataset/train/val_masks/image')
 
-    val_datagen = ImageDataGenerator(rescale=1. / 255)
-    val_image_generator = val_datagen.flow_from_directory('dataset/train/val_frames',
-                                                          target_size=IMAGE_SIZE,
-                                                          class_mode=None,
-                                                          batch_size=BATCH_SIZE,
-                                                          color_mode='grayscale',
-                                                          seed=SEED)
+    print('Create dataloader...')
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
 
-    val_mask_generator = val_datagen.flow_from_directory('dataset/train/val_masks',
-                                                         target_size=IMAGE_SIZE,
-                                                         class_mode=None,
-                                                         batch_size=BATCH_SIZE,
-                                                         color_mode='grayscale',
-                                                         seed=SEED)
+    dataloader = {"train": train_dataloader,
+                  "val":  validation_dataloader}
 
-    train_generator = zip(train_image_generator, train_mask_generator)
-    val_generator = zip(val_image_generator, val_mask_generator)
+    print('Initialize model...')
+    model = models.UNet(n_channels=1, n_classes=1)
+    model = model.to(device)
 
-    # build model
-    model = models.UNET(input_size=(256, 256, 1))
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = Adam(model.parameters(), lr=1e-4)
+    scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.2, patience=3)
 
-    # load pretrained
-    #model = load_model("model.h5", custom_objects={'mean_iou': metrics.mean_iou})
-
-
-    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy', metrics.mean_iou])
-
-    # configure callbacks
-    checkpoint = ModelCheckpoint("model.h5", verbose=1, save_best_only=True, save_weights_only=False,
-                                 monitor='val_mean_iou', mode='max')
-    earlystopping = EarlyStopping(patience=10, verbose=1, monitor='val_mean_iou', mode='max')
-    reduce_lr = ReduceLROnPlateau(factor=0.2, patience=3, verbose=1, min_delta=0.000001,
-                                  monitor='val_mean_iou', mode='max')
-    tensorboard = TensorBoard(log_dir='./logs/' + time.strftime("%Y%m%d_%H%M%S"), histogram_freq=0,
-                              write_graph=True, write_images=True)
-
-    # train model
-    model.fit_generator(train_generator, epochs=NO_OF_EPOCHS,
-                        steps_per_epoch=(NO_OF_TRAINING_IMAGES // BATCH_SIZE),
-                        validation_data=val_generator,
-                        validation_steps=(NO_OF_VAL_IMAGES // BATCH_SIZE),
-                        callbacks=[checkpoint, earlystopping, reduce_lr, tensorboard])
-
+    print('Start training...')
+    train(model, dataloader, criterion, optimizer, scheduler, num_epochs=NO_OF_EPOCHS)
 
 if __name__ == '__main__':
     main()
