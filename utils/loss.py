0d630adb479fb730b0903bd364f0e7abf58ab2d2
Tobias Steidle <tobias.steidle@softwaredev.de>
2021-02-04 08:44:36 +0100

    feat: transcribe loss and metrics to pytorch.

--- utils/loss.py
+++ utils/loss.py
@@ -1,30 +1,12 @@
-from keras import backend as K
-from utils import metrics
+import torch
 
-def dice_coef_loss(y_true, y_pred):
-    return 1 - dice_coef(y_true, y_pred)
 
-def iou_loss(true, pred):
-    intersection = true * pred
-    notTrue = 1 - true
-    union = true + (notTrue * pred)
-    return K.sum(intersection)/K.sum(union)
+def dice_coef(output, target):
+    smooth = 1e-5
 
-def jaccard_distance_loss(y_true, y_pred, smooth=100):
-    """
-    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
-            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+    output = torch.sigmoid(output).view(-1).data.cpu().numpy()
+    target = target.view(-1).data.cpu().numpy()
+    intersection = (output * target).sum()
 
-    The jaccard distance loss is usefull for unbalanced datasets. This has been
-    shifted so it converges on 0 and is smoothed to avoid exploding or disapearing
-    gradient.
-
-    Ref: https://en.wikipedia.org/wiki/Jaccard_index
-
-    @url: https://gist.github.com/wassname/f1452b748efcbeb4cb9b1d059dce6f96
-    @author: wassname
-    """
-    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
-    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
-    jac = (intersection + smooth) / (sum_ - intersection + smooth)
-    return (1 - jac) * smooth
+    return (2. * intersection + smooth) / \
+           (output.sum() + target.sum() + smooth)
--- utils/metrics.py
+++ utils/metrics.py
@@ -1,19 +1,24 @@
-import numpy as np
-import tensorflow as tf
-from keras import backend as K
+import torch
 
-def dice_coef(y_true, y_pred, smooth=1):
-    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
-    return (2. * intersection + smooth) / (K.sum(K.square(y_true), -1) + K.sum(K.square(y_pred), -1) + smooth)
 
-def mean_iou(y_true, y_pred):
-    prec = []
-    for t in np.arange(0.5, 1.0, 0.05):
-        y_pred_ = tf.to_int32(y_pred > t)
-        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
-        K.get_session().run(tf.local_variables_initializer())
-        with tf.control_dependencies([up_opt]):
-            score = tf.identity(score)
-        prec.append(score)
-    return K.mean(K.stack(prec), axis=0)
+def multi_acc(pred, label):
+    _, tags = torch.max(pred, dim=1)
+    corrects = (tags == label).float()
+    acc = corrects.sum() / corrects.numel()
+    acc = acc * 100
+    return acc
 
+
+def iou_score(output, target):
+    smooth = 1e-5
+
+    if torch.is_tensor(output):
+        output = torch.sigmoid(output).data.cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.data.cpu().numpy()
+    output_ = output > 0.5
+    target_ = target > 0.5
+    intersection = (output_ & target_).sum()
+    union = (output_ | target_).sum()
+
+    return (intersection + smooth) / (union + smooth)
